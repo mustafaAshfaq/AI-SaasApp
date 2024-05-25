8fde115d86f71a4cbea4b731dc634e9a11e038b8
mb <mustafa.ash@gmail.com>
2024-05-25 12:37:34 +0530

    Astro App changes done

---
+++ src/firebase/client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBdVKXtlKMil3jaoDkIPNoxHux3FQa5z6M",
+  authDomain: "saasapp1-8a615.firebaseapp.com",
+  projectId: "saasapp1-8a615",
+  storageBucket: "saasapp1-8a615.appspot.com",
+  messagingSenderId: "1053425985887",
+  appId: "1:1053425985887:web:bf594fb81ff4c9ab38eeea",
+  measurementId: "G-WGGDT3XZBY"
+  };
+export const app = initializeApp(firebaseConfig);
