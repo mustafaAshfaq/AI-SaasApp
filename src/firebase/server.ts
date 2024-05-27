33ecaec24827a273ce4128240574e42b578628c2
mb <mustafa.ash@gmail.com>
2024-05-27 20:51:27 +0530

    workaround for PEM formatting issue with firebaseconfig

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,12 +1,12 @@
 import type { ServiceAccount } from "firebase-admin";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
-
+import {Buffer} from 'node:buffer'
 const activeApps = getApps();
 const serviceAccount = {
   type: "service_account",
   project_id: import.meta.env.FIREBASE_PROJECT_ID,
   private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: JSON.parse(import.meta.env.FIREBASE_PRIVATE_KEY),
+  private_key: Buffer.from(import.meta.env.FIREBASE_PRIVATE_KEY_ENC,'base64').toString('utf8'),
   client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
   client_id: import.meta.env.FIREBASE_CLIENT_ID,
   auth_uri: import.meta.env.FIREBASE_AUTH_URI,
